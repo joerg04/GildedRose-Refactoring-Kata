d4fa472321652f44980d68187ee1eb09cd5f4c2d
joerg04 <2016@wglp.de>
2019-10-23 21:00:04 +0200

    (my) final solution

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,58 +1,105 @@
 package com.gildedrose
 
-class GildedRose(var items: Array<Item>) {
+class GildedRose(private var items: Array<Item>) {
 
-    fun updateQuality() {
-        for (i in items.indices) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+    companion object {
+        const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+        const val CHEESE = "Aged Brie"
+        const val TICKETS = "Backstage passes to a TAFKAL80ETC concert"
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+    fun updateQuality() = items.map(::intoWrapper).forEach(::updateItemQuality)
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
+    private fun updateItemQuality(item: ItemWrapper) {
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+        item.updateSellIn()
+        item.updateQuality()
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+        if (item.isOutdated()) {
+            item.updateQualityAfterOutdated()
         }
     }
 
+    private fun intoWrapper(item: Item) = when (item.name) {
+        CHEESE -> Cheese(item)
+        TICKETS -> Tickets(item)
+        SULFURAS -> Sulfuras(item)
+        else -> Regular(item)
+    }
 }
+
+interface ItemWrapper {
+    val internal: Item
+
+    fun updateSellIn()
+
+    fun isOutdated(): Boolean
+
+    fun updateQuality()
+
+    fun updateQualityAfterOutdated()
+}
+
+abstract class BaseItemWrapper(override val internal: Item) : ItemWrapper {
+
+    companion object {
+        const val MAX_QUALITY = 50
+        const val MIN_QUALITY = 0
+        const val DELTA_QUALITY = 1
+    }
+
+    override fun updateSellIn() {
+        internal.sellIn -= 1
+    }
+
+    final override fun isOutdated() = internal.sellIn < 0
+
+    override fun updateQuality() { // nOP
+    }
+
+    override fun updateQualityAfterOutdated() { // nOP
+    }
+
+    protected fun increaseQuality() {
+        if (internal.quality < MAX_QUALITY) {
+            internal.quality += DELTA_QUALITY
+        }
+    }
+
+    protected fun decreaseQuality() {
+        if (internal.quality > MIN_QUALITY) {
+            internal.quality -= DELTA_QUALITY
+        }
+    }
+}
+
+class Sulfuras(override val internal: Item) : BaseItemWrapper(internal) {
+    override fun updateSellIn() { /* nOP is legendary item */
+    }
+}
+
+class Regular(override val internal: Item) : BaseItemWrapper(internal) {
+    override fun updateQuality() = decreaseQuality()
+    override fun updateQualityAfterOutdated() = decreaseQuality()
+}
+
+class Cheese(override val internal: Item) : BaseItemWrapper(internal) {
+    override fun updateQuality() = increaseQuality()
+    override fun updateQualityAfterOutdated() = increaseQuality()
+}
+
+class Tickets(override val internal: Item) : BaseItemWrapper(internal) {
+    override fun updateQuality() {
+        var sellInGaps = internal.sellIn
+        while (sellInGaps < 15) {
+            increaseQuality()
+            sellInGaps += 5
+        }
+    }
+
+    override fun updateQualityAfterOutdated() {
+        internal.quality = MIN_QUALITY
+    }
+}
+
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,18 +1,158 @@
 package com.gildedrose
 
+import com.gildedrose.GildedRoseTest.ItemBuilder.Companion.DEFAULT_QUALITY
+import com.gildedrose.GildedRoseTest.ItemBuilder.Companion.tickets
+import com.gildedrose.GildedRoseTest.ItemBuilder.Companion.cheese
+import com.gildedrose.GildedRoseTest.ItemBuilder.Companion.regular
+import com.gildedrose.GildedRoseTest.ItemBuilder.Companion.sulfuras
 import org.junit.Assert.*
 import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+    sealed class ItemBuilder(private var name: String, private var sellIn: Int = DEFAULT_SELLIN, private var quality: Int = DEFAULT_QUALITY) {
 
+        companion object {
+            const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+            const val TICKET = "Backstage passes to a TAFKAL80ETC concert"
+            const val CHEESE = "Aged Brie"
+            const val REGULAR = "foo"
+
+            const val DEFAULT_SELLIN = 10
+            const val DEFAULT_QUALITY = 10
+
+            val sulfuras = Sulfuras()
+            val cheese = Cheese()
+            val tickets = Tickets()
+            val regular = Regular()
+        }
+
+        class Sulfuras : ItemBuilder(SULFURAS, 0, 80)
+        class Tickets : ItemBuilder(TICKET)
+        class Cheese : ItemBuilder(CHEESE)
+        class Regular : ItemBuilder(REGULAR)
+
+        fun item() = build()
+        fun outdated() = build().apply { sellIn = 0 }
+        fun highQuality() = build().apply { quality = 50 }
+        fun noQuality() = build().apply { quality = 0 }
+        fun quality(input: Int) = build().apply { quality = input }
+        fun sellIn(input: Int) = build().apply { sellIn = input }
+
+        private fun build() = Item(name, sellIn, quality)
     }
 
+    // tests regarding quality
+
+    @Test
+    fun ticketsShouldNotExceed50InQuality() {
+        checkItemQuality(50, tickets.highQuality())
+    }
+
+    @Test
+    fun ticketsShouldDropToZeroAfterSellInReached() {
+        checkItemQuality(0, tickets.outdated())
+    }
+
+    @Test
+    fun ticketsShouldIncrease_Stage3() {
+        checkItemQuality(DEFAULT_QUALITY + 3, tickets.sellIn(5))
+    }
+
+    @Test
+    fun ticketsShouldIncrease_Stage3HighQualityKeeps50() {
+        checkItemQuality(50, tickets.sellIn(5).apply { quality = 50 })
+    }
+
+    @Test
+    fun ticketsShouldIncrease_Stage2() {
+        checkItemQuality(DEFAULT_QUALITY + 2, tickets.item())
+    }
+
+    @Test
+    fun ticketsShouldIncrease_Stage2HighQualityKeeps50() {
+        checkItemQuality(50, tickets.item().apply { quality = 50 })
+    }
+
+    @Test
+    fun ticketsShouldIncrease_Stage1() {
+        checkItemQuality(DEFAULT_QUALITY + 1, tickets.sellIn(11))
+    }
+
+    // cheese
+
+    @Test
+    fun cheeseShouldNotExceed50InQualityOutdated() {
+        checkItemQuality(50, cheese.highQuality().apply { sellIn = -1 })
+    }
+
+    @Test
+    fun cheeseShouldNotExceed50InQuality2() {
+        checkItemQuality(50, cheese.quality(49))
+    }
+
+    @Test
+    fun cheeseShouldNotExceed50InQuality2Outdated() {
+        checkItemQuality(50, cheese.quality(48).apply { sellIn = -1 })
+    }
+
+    @Test
+    fun cheeseShouldNotExceed50InQuality3Outdated() {
+        checkItemQuality(50, cheese.quality(49).apply { sellIn = -1 })
+    }
+
+
+    @Test
+    fun regularShouldKeepZeroQualityEvenWhenOutdated() {
+        checkItemQuality(0, regular.noQuality().apply { sellIn = 0 })
+    }
+
+    @Test
+    fun regularShouldKeepZeroQuality() {
+        checkItemQuality(0, regular.noQuality())
+    }
+
+    @Test
+    fun regularShouldGiveDecreasedQuality() {
+        checkItemQuality(DEFAULT_QUALITY - 1, regular.item())
+    }
+
+    @Test
+    fun regularShouldGiveDecreasedQualityEvenWhenOutdated() {
+        checkItemQuality(DEFAULT_QUALITY - 2, regular.outdated())
+    }
+
+    @Test
+    fun sulfurasShouldNotIncrease() {
+        checkItemQuality(80, sulfuras.item().apply { sellIn = -1 })
+    }
+
+    // tests regarding sellIn
+
+    @Test
+    fun shouldGiveDecreasedSellIn() {
+        val item = regular.item()
+        val oldSellIn = item.sellIn
+        GildedRose(arrayOf(item)).updateQuality()
+        assertEquals(oldSellIn - 1, item.sellIn)
+    }
+
+    @Test
+    fun shouldGiveNoDecreasedSellInfForSulfuras() {
+        val item = sulfuras.item()
+        val oldSellIn = item.sellIn
+        GildedRose(arrayOf(item)).updateQuality()
+        assertEquals(oldSellIn, item.sellIn)
+    }
+
+
+    // helpers
+
+    private fun checkItemQuality(expectedQuality: Int, item: Item) {
+        val items = arrayOf(item)
+        GildedRose(items).updateQuality()
+        assertEquals(expectedQuality, item.quality)
+    }
 
 }
 
